6477066140440433867ae55aa49b922c8b32d954
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-01-25 16:51:15 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a;
+
+
+printf ("enter a value");
+scanf ("%d",&a);
+if (a>0)
+{
+printf ("a value is positive");
+}
+else if (a=0)
+{
+printf ("a is negative");
+}
+else 
+{
+printf ("a is zero");
+}
+}
