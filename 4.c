cd99e2de6041272ecd902a347ad8b8dc941156c2
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-01-30 16:27:39 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h> 
+void main() 
+{
+char n;
+clrscr();
+printf("Enter the character");
+scanf("%c",&n);
+if(n==a&&n==e&&n==i&&n==o&&u);
+{
+printf("Alfapht");
+{
+else
+}
+getch();
+}
