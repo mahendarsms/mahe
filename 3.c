798a0c02467ff2b8e572a901c044cc1ebd1fc1fc
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-01-30 16:09:55 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+clrscr();
+printf("Enter character letter");
+scanf("%c",&b);
+if(b=a!!b=A&&,b=e!!b=e!!b=E&&,b=i!! b=I&&,b=I&,b=o!!b=o&&b=U!!b=u)
+{ 
+printf("The character is vowel");
+}
+else
+{
+printf("The character is consoneat");
+}
+getch();
+}
