7423248347c9861e7bad5b24c1ab9c9d0bef8140
mahendarsms <35800528+mahendarsms@users.noreply.github.com>
2018-01-30 15:58:18 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("Enter a value");
+scanf("%d,&a);
+if ((a%2)==0);
+{
+printf("The a is even");
+}
+else 
+{
+printf("a is odd");
+}
+getch();
+}
